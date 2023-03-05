f2b88d3ae673d2ba2bb1a7b0e2de3b6337e3f4af
ti1ek.koszhanov <tilek.koszhanov@mail.ru>
2023-03-05 15:19:04 +0500

    Initial Commit

---
+++ Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by Zhuldyz Bukeshova on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
